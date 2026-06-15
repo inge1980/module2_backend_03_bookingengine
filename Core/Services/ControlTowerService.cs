4088548205cb9f7be4b44ab1512bfeef8c4aba02
Inge Pedersen <inge.pedersen.1980@gmail.com>
2026-06-15 13:05:24 +0200

    counter service test

--- Core/Services/ControlTowerService.cs
+++ Core/Services/ControlTowerService.cs
@@ -1,31 +1,7 @@
 using Core.Models;
 namespace Core.Services;
 
-public static class ThreadCounterService
+public static class ControlTowerService
 {
-    public static List<Thread> RunThreads(IEnumerable<Counter> counters)
-    {
-        List<Thread> threads = [];
-
-        foreach(var counter in counters)
-        {
-            var thread = new Thread(()=>Count(counter));
-            thread.Start();
-            threads.Add(thread);
-        }
-
-        return threads;
-    }
-
-    private static void Count(Counter counter)
-    {
-        Console.WriteLine($"Counter {counter.Name} started counting on thread....");
-
-        for (var i = 1; i <= counter.MaxVal; i++)
-        {
-            Thread.Sleep(counter.Delay);
-            Console.WriteLine($"Counted to {1} on {counter.Name}");
-        }
-        Console.WriteLine($"Counter {counter.Name} is complete...");
-    }
+    
 }
